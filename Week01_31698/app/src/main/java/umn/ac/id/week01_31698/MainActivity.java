3fad1f5320c9bdd99ac0df6d6d564e35a0267294
unknown <payudarasemut900@gmail.com>
2021-03-16 22:51:57 +0700

    First commit

---
+++ Week01_31698/app/src/main/java/umn/ac/id/week01_31698/MainActivity.java
@@ -0,0 +1,13 @@
+package umn.ac.id.week01_31698;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
